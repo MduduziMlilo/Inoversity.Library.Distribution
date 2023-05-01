f8fb219ace4dd46bac565462ccfd45e53e55cc2e
Victor Ikwuemesim <victorcomposes@outlook.com>
2023-05-01 18:06:01 +0200

    feature(integration-test) - allows us to test the api

---
+++ InoversityLibrary/InoversityLibrary.Infrastructure.Distribution.Tests/IntergrationTests/InoversityLibraryFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace InoversityLibrary.Infrastructure.Distribution.Tests.IntergrationTests;
+
+public class InoversityLibraryFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+    }
+}
